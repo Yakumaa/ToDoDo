af03122d8fa6c972b18721597b9202caee569179
bajra_sushant <bajra.sushant@gmail.com>
2023-07-31 16:03:41 +0545

    restart js completed rendering todos from array

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,25 +1,45 @@
-import * as model from './model.js';
+"use strict";
 
-import addTodoView from './todoView.js';
+const todoContainer = document.querySelector('.todo--container');
 
-const createBtn = document.getElementById('create__todo--btn');
-const todoList =  document.querySelector('.todo-list__list');
+let todos = [{
+  title: 'ToDoDo',
+  dueDate: '10-04-2023',
+  id: 'todo1',
+  tag: 'Project',
+}, {
+  title: 'ToDoDo 2',
+  dueDate: '10-14-2023',
+  id: 'todo2',
+  tag: 'Project',
 
-const controlAddTodo = function() {
-  const todo = addTodoView.getTodo();
+}]
 
-  if(!todo) return;
+const generateMarkup = function(data) {
+  return `
+							<div class='task' draggable='true'>
+							<div class='task__tags'>
+								<span class='task__tag task__tag--copyright'>${data.tag}</span>
+								<button class='task__add'><span class="material-icons-sharp">add</span></button>
+								<!-- Create new task button-->
+								<button class='task__options'><span class="material-icons-sharp">more_horiz</span></button>
+							</div>
+							<p>${data.title}</p>
+							<div class='task__stats'>
+								<!-- TODO: make the flag 12px -->
+								<span><time datetime="${data.dueDate}"><span class="material-icons-sharp">flag</span>${data.dueDate.slice(0,5)}</time></span>
+								<span><span class="material-icons-sharp">chat_bubble</span>3</span>
+								<span class='task__owner'></span> <!-- add profile picture here -->
+							</div>
+						</div>
+  `;
 
-  model.todos.push({
-    todo: todo.todoDetails,
-    deadline: todo.deadline,
-    id: todo.id,
-    status: 'todo',
-  });
-  addTodoView.render(model.todos);
 }
-
-const init = function() {
-  addTodoView.addHandlerAddTodo(controlAddTodo);
+const showTodo = function() {
+  const markup = todos.map(todo => generateMarkup(todo)).join('');
+  console.log(markup);
+  todoContainer.innerHTML = '';
+  todoContainer.insertAdjacentHTML('beforeend', markup);
 };
-init();
+
+window.addEventListener('load', showTodo);
--- src/js/model.js
+++
@@ -1,6 +0,0 @@
-export const todos = [{
-  todo: 'ToDoDo',
-  deadline: '2023-11-04',
-  id: 'id1',
-  status: 'todo',
-}];
