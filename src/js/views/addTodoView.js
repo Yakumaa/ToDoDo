6b6c87cce19b925c2cf239cb43ccf746b9f864d0
bajra_sushant <bajra.sushant@gmail.com>
2023-08-03 21:29:15 +0545

    refactored and modified create todo functionality

--- src/js/views/addTodoView.js
+++ src/js/views/addTodoView.js
@@ -1,34 +1,67 @@
 import View from "./View.js";
 
 class AddTodoView extends View {
-  _parentElement = document.querySelector('.input__container');
+  _parentElement = document.querySelector('.add-todo-container');
+  _addTodoButton = document.getElementById("add__todo-btn");
+  _underlay = document.querySelector('.underlay');
+  _createTodo = document.querySelector('.create__todo-btn');
+  _plusBtn = document.querySelector('#todo__plus-btn');
+
+  constructor() {
+    super();
+    this._addHandlerShowAddTodo();
+  };
 
   _clearInput() {
     this._parentElement.querySelector('#todo-desc').value = '';
     this._parentElement.querySelector('#deadline-date').value = '';
   };
 
+  _createCloseBtn() {
+    this._plusBtn.innerHTML = '';
+    this._plusBtn.innerText = 'close';
+    this._plusBtn.classList.remove('add');
+    this._plusBtn.style.color = "rgb(248 113 113)";
+  }
+
+  _createAddBtn() {
+    this._plusBtn.innerHTML = '';
+    this._plusBtn.innerText = 'add';
+    this._plusBtn.classList.add('add');
+    this._plusBtn.style.color = 'var(--light-grey)';
+  }
+
+
   getTodo() {
     const title = this._parentElement.querySelector('#todo-desc').value;
     const dueDate = this._parentElement.querySelector('#deadline-date').value;
-    console.log(dueDate);
 
     this._clearInput();
-
+    this._toggleAddTodo();
     if (!title) return;
-
+    console.log(title, dueDate);
     return {
       title: title,
-      dueDate: dueDate,  
+      dueDate: dueDate,
       id: 'id' + new Date().getTime(),
-      tag: 'Practice', 
+      tag: 'Practice',
     }
   }
 
   addHandlerClick(handler) {
-    // this._parentElement.querySelector('#create__todo--btn').addEventListener('click', handler);
-    this._parentElement.querySelector('#add-btn').addEventListener('click', handler);
+    this._createTodo.addEventListener('click', handler);
   }
+
+  _toggleAddTodo() {
+    this._plusBtn.classList.contains('add') ? this._createCloseBtn() : this._createAddBtn();
+    this._underlay.classList.toggle('hidden');
+
+  };
+
+  _addHandlerShowAddTodo() {
+    this._addTodoButton.addEventListener('click', this._toggleAddTodo.bind(this));
+  }
+
 };
 
 export default new AddTodoView();
