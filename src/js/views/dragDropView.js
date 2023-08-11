7597b81fc00c0c84e2efe90563e427857aa50d57
bajra_sushant <bajra.sushant@gmail.com>
2023-08-11 13:51:29 +0545

    done dragdrop needs empty container fix and status update

--- src/js/views/dragDropView.js
+++ src/js/views/dragDropView.js
@@ -1,6 +1,11 @@
-class DragDropView {  
+class DragDropView {
   _parentElement = document.querySelector('.project-tasks');
   _containers = document.querySelectorAll('.containers');
+
+
+  constructor() {
+    this.addHandlerDragOver();
+  }
 
   printItem() {
     console.log(this._containers);
@@ -8,9 +13,48 @@ class DragDropView {
   addHandlerDrag() {
     this._parentElement.addEventListener('dragstart', e => {
       const dragging = e.target.closest('.draggable');
+      dragging.classList.add('dragging');
+      console.log(dragging);
+    });
+
+    this._parentElement.addEventListener('dragend', e => {
+      const dragging = e.target.closest('.draggable');
+      dragging.classList.remove('dragging');
       console.log(dragging);
     });
   }
+
+  addHandlerDragOver() {
+    this._containers.forEach(container => {
+      container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const afterElement = this._getDragAfterElement(container, e.clientY)
+        const draggable = document.querySelector('.dragging')
+        if (afterElement == null) {
+          container.appendChild(draggable)
+        } else {
+          container.insertBefore(draggable, afterElement)
+        }
+      });
+    });
+  }
+
+  _getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+
+  }
+
+
 };
 
 export default new DragDropView();
